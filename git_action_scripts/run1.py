f2011c1fcf35ee6a0d30fcc9606eb4e75cb35830
dezhaoliu <extern.dezhao.liu@cariad-technology.cn>
2025-07-03 16:14:23 +0800

    run a script

--- git_action_scripts/run1.py
+++ git_action_scripts/run1.py
@@ -11,14 +11,14 @@ if __name__ == '__main__':
     if project_root not in sys.path:
         sys.path.insert(0, project_root) # 将项目根目录添加到搜索路径的最前面
 
-    from utils.jira_utils import JiraUtils
+    from automation.utils.jira_utils import JiraUtils
+    server=os.environ.get('JIRA_SERVER')
+    print("jira server is ",server)
     jira_utils = JiraUtils(
         server=os.environ.get('JIRA_SERVER'),
         username=os.environ.get('JIRA_USERNAME'),
         api_token=os.environ.get('JIRA_API_TOKEN')
     )
-
-
     print("jira_util imported successfully!")
     # issue_key = os.environ.get('ISSUE_KEY')
     # issue = jira_utils.get_test_execution_by_key(issue_key)
